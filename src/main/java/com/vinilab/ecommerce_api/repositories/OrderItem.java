da06615a431b4eb61096d6c5a2c2d4528ed4dfa5
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-02 18:18:02 -0300

    feat: Order item repository created.

---
+++ src/main/java/com/vinilab/ecommerce_api/repositories/OrderItem.java
@@ -0,0 +1,6 @@
+package com.vinilab.ecommerce_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItem extends JpaRepository<OrderItem, Long> {
+}

da2606f23dc5a7a7d2bf5efd81c9cb404312e13f
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-02 17:10:43 -0300

    new project first commit

---
+++ src/main/java/com/vinilab/ecommerce_api/entities/Client.java
@@ -0,0 +1,12 @@
+package com.vinilab.ecommerce_api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Table(name = "client")
+@Entity
+public class Client implements Serializable {
+    private
+}

8711cfee1dd6165544fde0e8b9bb70be5d54b351
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-02 18:18:12 -0300

    feat: payment repository created.

---
+++ src/main/java/com/vinilab/ecommerce_api/repositories/Payment.java
@@ -0,0 +1,6 @@
+package com.vinilab.ecommerce_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Payment extends JpaRepository<Payment, Long> {
+}

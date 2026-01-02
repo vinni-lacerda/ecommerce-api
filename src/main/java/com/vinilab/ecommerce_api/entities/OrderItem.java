c097f8bdd1bb11f9b2e7e839dc850bd2a361edc0
vinni-lacerda <vini.lacerda@hotmail.com>
2026-01-02 18:01:35 -0300

    feat: order item entity created.

---
+++ src/main/java/com/vinilab/ecommerce_api/entities/OrderItem.java
@@ -0,0 +1,44 @@
+package com.vinilab.ecommerce_api.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+    private BigDecimal unitPrice;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
